2caafb99ad704a5243aaa074f001fc1fb8610c27
mohanhn5 <>
2021-05-27 13:06:09 +0000

    Completed

--- Program_5.c
+++ Program_5.c
@@ -5,13 +5,19 @@
 
 int main()
 {
-    int arr[10], n, i;
+    int arr[10], n, i,lar,sma;
     scanf("%d", &n);
 
     for(i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
-    //Write your code here
-  
+//Write your code here
+    for(i=0;i<n;i++)
+    {
+      if(arr[i]>lar)
+        lar=arr[i];
+      if(arr[i]<sma)
+        sma=arr[i];
+    }
+  printf("Largest=%d and Smallest=%d",lar,sma);
     return 0;
 }
